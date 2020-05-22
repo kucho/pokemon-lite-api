0ebf02704d3a4f5b53d01bb9ecd63ea99c9bb055
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-05-22 17:02:06 -0500

    Added nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :trainers
+  resources :trainers do
+    resources :pokemons
+  end
+
+  resources :pokemons do
+    resources :trainers
+  end
 end
