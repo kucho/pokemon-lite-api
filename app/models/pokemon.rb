a39fdf56805eca38f49c32c2dee791b348e10fa3
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-05-21 21:10:34 -0500

    Fix many to many relations

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,4 @@
 class Pokemon < ApplicationRecord
-   has_many :pokemon_trainer
-   has_many :trainers, through: :pokemon_trainer 
+   has_many :pokemon_trainers
+   has_many :trainers, through: :pokemon_trainers
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,2 +1,4 @@
 class Trainer < ApplicationRecord
+  has_many :pokemon_trainers
+  has_many :pokemons, through: :pokemon_trainers
 end
