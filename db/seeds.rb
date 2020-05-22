0bd95daad794089733d33faadfce20c35763b387
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-05-21 23:24:24 -0500

    Loaded data from csv to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv = CSV.read('./db/initial-data.csv', headers: true)
+
+csv.each do |row|
+  t_id = Trainer.find_or_create_by(
+    {
+      name: row['t_name'],
+      gender: row['t_gender'],
+      home_region: row['t_region'],
+      team_member_status: row['t_team_member'],
+      wins: row['wins'],
+      losses: row['losses']
+    }
+  ).id
+
+  p_id = Pokemon.find_or_create_by(
+    {
+      name: row['name'],
+      base_experience: row['base_exp'],
+      main_type: row['main_type'],
+      main_ability: row['main_ability']
+    }
+  ).id
+
+  PokemonTrainer.find_or_create_by(trainer_id: t_id, pokemon_id: p_id)
+end
+
+puts 'Data ready!'
