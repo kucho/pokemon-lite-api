01ddb25e435e640871778812ec31a25b4e26fe28
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-05-21 22:37:45 -0500

    Add table validations

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,13 @@
 class Pokemon < ApplicationRecord
    has_many :pokemon_trainers
    has_many :trainers, through: :pokemon_trainers
+
+   validates :name, presence: true, uniqueness: true
+   validates :main_ability, presence: true
+   validates :main_type,
+             inclusion: { in: ['bug', 'electric', 'fairy',
+                               'fighting', 'fire', 'flying',
+                               'grass', 'ground', 'ice',
+                               'normal', 'poison', 'psychic',
+                               'rock', 'steel', 'water']}
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,4 +1,9 @@
 class Trainer < ApplicationRecord
   has_many :pokemon_trainers
   has_many :pokemons, through: :pokemon_trainers
+
+  validates :name, presence: true, uniqueness: true
+  validates :gender, presence: true
+  validates :team_member_status, inclusion: { in: [true, false] }
+  validates :wins, :losses, numericality: { greater_than_or_equal_to: 0 }
 end
